dcac63f6a4aca374f6f96352ea395b143e5e64e4
Anirudh Kalia <anikal2001@gmail.com>
2018-12-14 18:54:21 -0500

    First Commit

---
+++ scripts.js
@@ -0,0 +1,38 @@
+
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/*
+function dropdown() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+*/
+
+window.onscroll = function() {menuScroll()};
+function menuScroll(){
+    if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+        document.getElementById("topnav").className = "test";
+        var x = document.getElementById("topnav");
+        var y = x.getElementsByTagName("a");
+        var i;
+        for (i = 0; i < y.length; i++) {
+            y[i].style.color = "black";
+            y[i].style.fontWeight = "600";
+        }
+ 
+        }else {
+        document.getElementById("topnav").className = "test1";
+        var x = document.getElementById("topnav");
+        var y = x.getElementsByTagName("a");
+        var i;
+        for (i = 0; i < y.length; i++) {
+            y[i].style.color = "white";
+            y[i].style.fontWeight = "600";
+        }
+        }
+    }
